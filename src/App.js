8851e30b8f3fc8b6da0a267a23945657530f7a6b
KIM_MS <78401083+sstaar91@users.noreply.github.com>
2022-12-15 19:05:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app">
-      <h1>컴포넌트 만들기</h1>
+      <h1 className="title">컴포넌트 만들기</h1>
       <div>card 컴포넌트 위치</div>
     </div>
   );
