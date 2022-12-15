31786f5e3c4321ae93306260357a33699e77fb57
sstaar91 <sstaar91@gmail.com>
2022-12-15 18:53:35 +0900

    ADD: 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <span>컴포넌트 만들기!</span>
+    <div className="app">
+      <h1>컴포넌트 만들기</h1>
+      <div>card 컴포넌트 위치</div>
     </div>
   );
 };
